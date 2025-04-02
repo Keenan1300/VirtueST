d1829a20804cab4bab7d106b54975925d50a1d64
hamilt25 <hamilt25@OA-S145-04>
2025-04-02 15:27:10 -0400

    Added level

--- Scenes/CharlieDialogue1.gd
+++ Scenes/CharlieDialogue1.gd
@@ -1,7 +1,7 @@
 extends StaticBody3D
 @onready var Charlie1Camera: Camera3D = $Charlie1Camera
 @onready var donedialogue1 = false
-
+@onready var cinematicbars = $CanvasLayer
 
 func _ready():
 	Dialogic.VAR.Charlie1Interact = true
@@ -9,14 +9,16 @@ func _ready():
 func _process(delta):
 	
 	if (Dialogic.VAR.Charlie1Interact == false):
+		cinematicbars.hide();
 		endinteraction()
 		print("its false")
 	else:
 		pass
 
 func interact():
+	cinematicbars.show();
 	$Charlie1Camera.current = true
-	
+	Dialogic.VAR.Charlie1Interact = true
 	##Dialogic thing goes here
 	
 	Dialogic.start("CharlieBones1")
---
+++ Scenes/Portal1.gd
@@ -0,0 +1,5 @@
+extends StaticBody3D
+
+func interact():
+	get_tree().change_scene_to_file("res://Scenes/World1Claudma.tscn")
+	print("INTERACTED WITH") 
